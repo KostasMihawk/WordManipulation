43d4943d9117a82b1bc000ca21a2bdfe2f8b387b
kwsmichopoulos@gmail.com <kwsmichopoulos@gmail.com>
2021-09-01 14:12:55 +0300

    Refactor part 1 -- Added LoanerInvtitationModel --

--- WordManipulation/Controllers/LoanerInvitationController.cs
+++ WordManipulation/Controllers/LoanerInvitationController.cs
@@ -28,8 +28,7 @@ namespace WordManipulation.Controllers
             return View();
         }
 
-        [HttpPost()]
-        //public ActionResult CreateLoanerInvitation(int bailif = 1, int notary = 1, string docNum = "123/20", string ofeileths = "γιαννης", string place = "", int gender = 0, int keao = 0, bool praxh = false, bool isPinakas = false, bool isBankEmpty = false)
+        [HttpPost()]       
         public ActionResult CreateLoanerInvitation(LoanerInvitationViewmodel vm)
         {
             DropDownGenerator generator = new DropDownGenerator();
@@ -49,27 +48,7 @@ namespace WordManipulation.Controllers
             form.KEAO = vm.KEAO;
             form.IsPinakas = vm.IsTable;
             form.IsBankEmpty = vm.IsBank;
-            form.KEAO = vm.KEAO;
-            //ylopoihsh me dictionary h tupple
-            //se ka8e adikeimeno st lista na fortwnei diaforetiko bool(form.praxh)
-            Dictionary<string, LegalEntity> foreis = new Dictionary<string, LegalEntity> {
-                { "προς το Ν.Π.Δ.Δ. με την επωνυμία e-ΕΦΚΑ που εδρεύει στην Αθήνα και εκπροσωπείται νόμιμα", LegalEntity.EFka},
-                { "προς τον κ. Διοικητή Της Ανεξάρτητης Αρχής Δημοσίων Εσόδων (Α.Α.Δ.Ε.), ως εκπρόσωπο του Ελληνικού Δημοσίου, κατοικοεδρεύοντα στην Αθήνα", LegalEntity.EllinikoDimosio },
-                { "προς την επισπεύδουσα ανώνυμη τραπεζική εταιρεία με την επωνυμία «Εθνική Τράπεζα της Ελλάδος Α.Ε.», που εδρεύει στην Αθήνα και εκπροσωπείται νόμιμα με ΑΦΜ 094014201", LegalEntity.Trapezes },
-                { "προς τον προϊστάμενο της αναγγελθείσας ΔΟΥ ",LegalEntity.LoipesUpirisies },
-                { "προς το " + EpilogiKeaoA(form.KEAO) + "Περιφερειακό ΚΕΑΟ " + EpilogiKeaoB(form.KEAO) +", ", LegalEntity.LoipesUpirisies },
-                { "προς "  + s.EpiloghGenousPanw(form.Gender) +  vm.Debtor, LegalEntity.FusikoProswpo },
-                { "_" ,LegalEntity.Empty},
-                //{"αναγγελθείσα ανώνυμη τραπεζική εταιρεία με την επωνυμία «INTRUM HELLAS ΑΝΩΝΥΜΗ ΕΤΑΙΡΕΙΑ ΔΙΑΧΕΙΡΙΣΗΣ ΑΠΑΙΤΗΣΕΩΝ ΑΠΟ ΔΑΝΕΙΑ ΚΑΙ ΠΙΣΤΩΣΕΙΣ» και το διακριτικό τίτλο «INTRUM HELLAS ΑΕΔΑΔΠ», όπως μετονομάστηκε από «ALTERNATIVE FINANCIAL SOLUTIONS ΜΟΝΟΠΡΟΣΩΠΗ ΑΝΩΝΥΜΗ ΕΤΑΙΡΕΙΑ ΔΙΑΧΕΙΡΙΣΗΣ ΑΠΑΙΤΗΣΕΩΝ ΑΠΟ ΔΑΝΕΙΑ ΚΑΙ ΠΙΣΤΩΣΕΙΣ», που εδρεύει στην Αθήνα και εκπροσωπείται νόμιμα, ως διαχειρίστρια των απαιτήσεων της ανώνυμης τραπεζικής εταιρείας με την επωνυμία «ΤΡΑΠΕΖΑ ΠΕΙΡΑΙΩΣ Α.Ε», που εδρεύει στην Αθήνα και εκπροσωπείται νόμιμα, η οποία είχε καταστεί οιονεί καθολική διάδοχος των περουσιακών στοιχείων της «ΓΕΝΙΚΗ ΤΡΑΠΕΖΑ ΤΗΣ ΕΛΛΑΔΟΣ Α.Ε», ",LegalEntity.Trapezes } 
-            };
-
-
-
-            //form = agent.TranslateFormVm(vm);
-            //agent.ManageText(form, vm.DocumentEnum);
-
-            var EnhacedList = generator.GetBanks();
-
+            form.KEAO = vm.KEAO;  
 
             form.Defender = new Defender();
 
@@ -95,33 +74,8 @@ namespace WordManipulation.Controllers
                 }
                 return File(stream.ToArray(), "application/zip", "test.zip");
             }
-           
-
-
-
-
         }
 
-        public string EpilogiKeaoA(KEAO keao)
-        {
-            if (keao == KEAO.AAthina)
-                return " A ";
-            else if (keao == KEAO.BAthina)
-                return " B ";
-            else
-                return "";
-        }
-
-        public string EpilogiKeaoB(KEAO keao)
-        {
-            if (keao == KEAO.AAthina)
-                return " Αθήνας";
-            else if (keao == KEAO.BAthina)
-                return " Αθήνας";
-            else if (keao == KEAO.Peireas)
-                return " Πειραιά";
-            else
-                return " Ελευσίνας";
-        }
+       
     }
 }
---
+++ WordManipulation/Models/LoanerInvitationModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WordManipulation.ViewModels;
+
+namespace WordManipulation.Models
+{
+    public class LoanerInvitationModel
+    {
+        public Baillif Baillif { get; set; }
+        public Attorney Notary { get; set; }
+        public Zone Zone { get; set; }
+        public int CaseNumber { get; set; }
+        public string Debtor { get; set; }
+        public Gender Gender { get; set; }
+        public string Location { get; set; }
+        public string Praxh { get; set; }
+        public KEAO KEAO { get; set; }
+        public bool IsTable { get; set; }
+        public bool IsBank { get; set; }
+        public bool? IsFusikoProsopo { get; set; }
+        public string Address { get; set; }
+        public string Upiresia { get; set; }
+        public string KeimenoPraxis { get; set; }
+
+    }
+}
