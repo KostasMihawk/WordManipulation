02aee48919e8a33a9d0efeff7811391382dea1d8
kwsmichopoulos@gmail.com <kwsmichopoulos@gmail.com>
2022-07-05 16:01:34 +0300

    added dovalue and cepal folders

--- WordManipulation/Controllers/LoanerInvitationController.cs
+++ WordManipulation/Controllers/LoanerInvitationController.cs
@@ -52,6 +52,14 @@ namespace WordManipulation.Controllers
                         {    
                             zip.AddEntry("Εφορίες/" + doc.Name + ".docx", s.CreateSunexisiPlistiriasmou(LoanerInvitationModel));
                         }
+                        else if(doc.Name.Contains("Cepal"))
+                        {
+                            zip.AddEntry("Cepal/" + doc.Name + ".docx", s.CreateSunexisiPlistiriasmou(LoanerInvitationModel));
+                        }
+                        else if (doc.Name.Contains("doValue"))
+                        {
+                            zip.AddEntry("doValue/" + doc.Name + ".docx", s.CreateSunexisiPlistiriasmou(LoanerInvitationModel));
+                        }
                         else
                         {
                             zip.AddEntry(doc.Name + ".docx", s.CreateSunexisiPlistiriasmou(LoanerInvitationModel));
--- WordManipulation/Controllers/SunexisiPlistriasmouController.cs
+++ WordManipulation/Controllers/SunexisiPlistriasmouController.cs
@@ -43,6 +43,14 @@ namespace WordManipulation.Controllers
                         {      
                             zip.AddEntry("Εφορίες/" + doc.Name + ".docx", s.CreateEntoliSunexisisPlistiriasmou(model));
                         }
+                        else if (doc.Name.Contains("Cepal"))
+                        {
+                            zip.AddEntry("Cepal/" + doc.Name + ".docx", s.CreateEntoliSunexisisPlistiriasmou(model));
+                        }
+                        else if (doc.Name.Contains("doValue"))
+                        {
+                            zip.AddEntry("doValue/" + doc.Name + ".docx", s.CreateEntoliSunexisisPlistiriasmou(model));
+                        }
                         else
                         {
                             zip.AddEntry(doc.Name + ".docx", s.CreateEntoliSunexisisPlistiriasmou(model));
